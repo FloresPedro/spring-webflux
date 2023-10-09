2a8c0c899a88aacbe50f65351de44356d676defe
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-10-09 17:41:26 -0600

    manejo de la contrapresion en el controlador

--- src/main/java/com/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,11 +1,14 @@
 package com.springboot.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.springboot.webflux.app.models.dao.IProductoDao;
 import com.springboot.webflux.app.models.documents.Producto;
@@ -18,6 +21,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner{
 	@Autowired
 	private IProductoDao dao;
 	
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
+	
 	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 
 	public static void main(String[] args) {
@@ -27,6 +33,10 @@ public class SpringBootWebfluxApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		//Borrar los productos insertados cada vez que se inicie la aplicacion
+		mongoTemplate.dropCollection("productos").subscribe();
+		
 		Flux.just(new Producto("TV Panasonic Pantalla LCD", 456.3),
 				new Producto("Sony Camara HD Digital", 150.2),
 				new Producto("Apple iPod", 49.8),
@@ -36,7 +46,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner{
 				new Producto("HP NoteBook Omen 17", 2500.48),
 				new Producto("Mica Comoda 5 Cajones ", 70.56),
 				new Producto("TV Sony Bravia OLED 4K Ultra HD", 3550.20)
-				).flatMap(producto -> dao.save(producto))
+				).flatMap(producto -> {
+					producto.setCreateAt(new Date());
+					return dao.save(producto);})
 		.subscribe(producto -> log.info("Insert: " + producto.getId() +" "+ producto.getNombre()));
 		
 	}
---
+++ src/main/java/com/springboot/webflux/app/controllers/ProductoController.java
@@ -0,0 +1,52 @@
+package com.springboot.webflux.app.controllers;
+
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+
+import com.springboot.webflux.app.models.dao.IProductoDao;
+import com.springboot.webflux.app.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
+
+@Controller
+public class ProductoController {
+	
+	@Autowired
+	private IProductoDao dao;
+	
+	private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+	
+	@GetMapping("/listar")
+	public String listar(Model model) {
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		});
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de Productos");
+		return "listar";
+	}
+	
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).delayElements(Duration.ofSeconds(1)); //Tiempo de espera que se publican elementos
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 2));
+		model.addAttribute("titulo", "Listado de Productos");
+		return "listar";
+	}
+
+}
