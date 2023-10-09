37db36dcc216e17d9a0ad41ae007c54fda70bc83
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-10-09 14:05:01 -0600

    conexion con la base de datos e insercion de productos

---
+++ src/main/java/com/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -0,0 +1,44 @@
+package com.springboot.webflux.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.springboot.webflux.app.models.dao.IProductoDao;
+import com.springboot.webflux.app.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootWebfluxApplication implements CommandLineRunner{
+	
+	@Autowired
+	private IProductoDao dao;
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux.just(new Producto("TV Panasonic Pantalla LCD", 456.3),
+				new Producto("Sony Camara HD Digital", 150.2),
+				new Producto("Apple iPod", 49.8),
+				new Producto("Sony NoteBook", 846.49),
+				new Producto("Hewlett Packard Multifuncional", 200.2),
+				new Producto("Bianchi Bicicleta", 70.50),
+				new Producto("HP NoteBook Omen 17", 2500.48),
+				new Producto("Mica Comoda 5 Cajones ", 70.56),
+				new Producto("TV Sony Bravia OLED 4K Ultra HD", 3550.20)
+				).flatMap(producto -> dao.save(producto))
+		.subscribe(producto -> log.info("Insert: " + producto.getId() +" "+ producto.getNombre()));
+		
+	}
+
+}
